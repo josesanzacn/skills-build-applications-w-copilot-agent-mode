a665cb2b72b27a14e66109349b1799aa19b40108
josesanzacn <jose.sanz@accenture.com>
2025-10-28 17:36:35 +0000

    Actualización de URLs y hosts para Codespace y localhost en Django

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-cch7xie$gwo(1+9#y%mqw2x7c1#75q-0t!-=p7!b6%0o8u^eta
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
